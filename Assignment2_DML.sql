c380fc5aae70de968489d516b4493b6adffe75e0
Kevin Le <89573171+kevle6@users.noreply.github.com>
2023-12-16 01:39:44 -0800

    Added DML file

---
+++ Assignment2_DML.sql
@@ -0,0 +1,60 @@
+INSERT INTO fse_engstore.Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (1, 'Eco Shopper', 'Alice Johnson', '789 Eco Ave', 'EcoTown', 'ET789', 'EcoCountry');
+INSERT INTO fse_engstore.Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (2, 'Green Buyer', 'Bob Brown', '321 Green St', 'EcoVille', 'EV321', 'EcoLand');
+INSERT INTO fse_engstore.Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (3, 'Eco Inc.', 'John Doe', '1234 West Town', 'Orange', '45642', 'Saudi Arabia');
+INSERT INTO fse_engstore.Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (4, 'Anti-Eco Club', 'Pete the Panther', '1 University Drive', 'Orange', '92866', 'USA');
+INSERT INTO fse_engstore.Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (5, 'Cal State Long Beach', 'Elbee the Shark', '1250 Bellflower Blvd', 'Long Beach', '90840', 'Sweden');
+INSERT INTO fse_engstore.Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (6, 'Vegan Lady', 'Kadie Karen Diekmeyer', '44444 Tiktok Lane', 'TikTok', '67656', 'EcoLand');
+INSERT INTO fse_engstore.Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (7, 'Kevin Le', 'Kevin Le', '555 Fire Disc Ave', 'Santa Ana', '11123', 'USA');
+INSERT INTO fse_engstore.Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (8, 'CSEENG Company', 'Michael Smith', 'Tulip Park', 'EcoVille', '55555', 'Alaska');
+INSERT INTO fse_engstore.Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (9, 'Disney', 'Walt Disney', 'Disneyland', 'Anaheim', '92802', 'USA');
+INSERT INTO fse_engstore.Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES (10, 'Stark Industries', 'Tony Stark', '10880 Malibu Point', 'Malibu', '90265', 'USA');
+
+INSERT INTO fse_engstore.Suppliers (SupplierID, SupplierName, ContactName, Address, City, PostalCode, Country, Phone) VALUES (1, 'EcoFriendly Ltd', 'John Doe', '123 Green Road', 'EcoCity', 'EC123', 'Ecoland', '123-456-7890');
+INSERT INTO fse_engstore.Suppliers (SupplierID, SupplierName, ContactName, Address, City, PostalCode, Country, Phone) VALUES (2, 'NatureGoods Inc', 'Jane Smith', '456 Natural Way', 'GreenVille', 'GV456', 'Greenland', '987-654-3210');
+INSERT INTO fse_engstore.Suppliers (SupplierID, SupplierName, ContactName, Address, City, PostalCode, Country, Phone) VALUES (3, 'FSE Supplier', 'Rene German', '456 Red Lane', 'FlavorTown', '11111', 'Ecoland', '445-890-7678');
+INSERT INTO fse_engstore.Suppliers (SupplierID, SupplierName, ContactName, Address, City, PostalCode, Country, Phone) VALUES (4, 'FeelGood Productions', 'Steven Wozniak', '10 West Way', 'Commerce', '43927', 'Greenland', '888-333-2222');
+INSERT INTO fse_engstore.Suppliers (SupplierID, SupplierName, ContactName, Address, City, PostalCode, Country, Phone) VALUES (5, 'Chapman Ltd', 'Daniele Struppa', '1 University Dr.', 'Orange', '92688', 'USA', '123-123-1234');
+INSERT INTO fse_engstore.Suppliers (SupplierID, SupplierName, ContactName, Address, City, PostalCode, Country, Phone) VALUES (6, 'Students Unite', 'Jerry Price', '4545 Unknown St', 'Fort Lauderdale', '44444', 'Redlands', '333-333-3333');
+INSERT INTO fse_engstore.Suppliers (SupplierID, SupplierName, ContactName, Address, City, PostalCode, Country, Phone) VALUES (7, 'Lucas Productions', 'George Lucas', 'Star Wars Galaxy', 'Hollywood', '10298', 'Croissant', '444-555-6665');
+INSERT INTO fse_engstore.Suppliers (SupplierID, SupplierName, ContactName, Address, City, PostalCode, Country, Phone) VALUES (8, 'Saul Goodman Law Firm', 'Saul Goodman', 'Justice Circle', 'New Mexico', '88888', 'USA', '888-888-8888');
+INSERT INTO fse_engstore.Suppliers (SupplierID, SupplierName, ContactName, Address, City, PostalCode, Country, Phone) VALUES (9, 'Catan Ltd', 'John Wick', '788 Tries Total Lane', 'Bakersfield', '09998', 'Iceland', '777-121-1342');
+INSERT INTO fse_engstore.Suppliers (SupplierID, SupplierName, ContactName, Address, City, PostalCode, Country, Phone) VALUES (10, 'ACME Inc', 'Bugs Bunny', '2019 Warner Bros', 'Lakewood', '19283', 'USA', '122-432-1222');
+
+INSERT INTO fse_engstore.Products (ProductID, ProductName, SupplierID, Category, UnitPrice, UnitsInStock) VALUES (1, 'Bamboo Toothbrush', 1, 'Personal Care', 2.99, 97);
+INSERT INTO fse_engstore.Products (ProductID, ProductName, SupplierID, Category, UnitPrice, UnitsInStock) VALUES (2, 'Reusable Water Bottle', 1, 'Outdoor', 10.50, 200);
+INSERT INTO fse_engstore.Products (ProductID, ProductName, SupplierID, Category, UnitPrice, UnitsInStock) VALUES (3, 'Organic Cotton T-shirt', 2, 'Clothing', 15.99, 150);
+INSERT INTO fse_engstore.Products (ProductID, ProductName, SupplierID, Category, UnitPrice, UnitsInStock) VALUES (4, 'Salt Toothpaste', 3, 'Personal Care', 4.99, 100);
+INSERT INTO fse_engstore.Products (ProductID, ProductName, SupplierID, Category, UnitPrice, UnitsInStock) VALUES (5, 'Baseball Cap', 4, 'Clothing', 4.50, 200);
+INSERT INTO fse_engstore.Products (ProductID, ProductName, SupplierID, Category, UnitPrice, UnitsInStock) VALUES (6, 'iPhone 36', 4, 'Technology', 2999.99, 150);
+INSERT INTO fse_engstore.Products (ProductID, ProductName, SupplierID, Category, UnitPrice, UnitsInStock) VALUES (7, 'Ceramic Mug', 5, 'Kitchenware', 18.99, 100);
+INSERT INTO fse_engstore.Products (ProductID, ProductName, SupplierID, Category, UnitPrice, UnitsInStock) VALUES (8, 'Laptop', 5, 'Technology', 1999.99, 200);
+INSERT INTO fse_engstore.Products (ProductID, ProductName, SupplierID, Category, UnitPrice, UnitsInStock) VALUES (9, 'Jeans', 6, 'Clothing', 50.99, 150);
+INSERT INTO fse_engstore.Products (ProductID, ProductName, SupplierID, Category, UnitPrice, UnitsInStock) VALUES (10, 'DIY Fidget Spinner', 7, 'Toy', 29.99, 500);
+
+insert into fse_engstore.Orders (OrderID, CustomerID, OrderDate, ShipDate, ShipAddress, ShipCity, ShipPostalCode, ShipCountry)
+values  (1, 1, '2023-11-01', '2023-11-05', '789 Eco Ave', 'EcoTown', 'ET789', 'EcoCountry'),
+        (2, 2, '2023-11-03', '2023-11-08', '321 Green St', 'EcoVille', 'EV321', 'EcoLand'),
+        (3, 9, '2023-08-23', '2023-09-23', 'New Shipping Address', 'City of Ships', 'EV10294', 'Country of Ships'),
+        (4, 8, '2009-04-15', '2010-07-01', 'BoomTown', 'Foobar', '39405', 'Middle Earth'),
+        (5, 5, '1992-01-23', '2001-04-04', 'Zoomland', 'Zoolander', '019234', 'Greenland'),
+        (6, 6, '2006-06-06', '1992-06-06', 'Hollywood Hills 1992', 'Hollywood', '01923', 'America'),
+        (7, 10, '2022-01-01', '2022-01-02', '10880 Malibu Way', 'Malibu', '90265', 'USA'),
+        (8, 3, '1993-03-03', '2003-03-03', 'Sriracha Factory', 'Napa', '923343', 'USA'),
+        (9, 8, '3948-02-02', '2000-04-24', '44444 TikTok Way', 'TikTok', '67656', 'Alaska'),
+        (10, 1, '2023-12-15', '2023-12-15', 'Chapman University', 'Orange', '92019', 'USA');
+
+insert into fse_engstore.OrderDetails (OrderDetailID, OrderID, ProductID, Quantity, UnitPrice)
+values  (1, 1, 1, 2, 2.99),
+        (2, 1, 3, 1, 15.99),
+        (3, 2, 2, 1, 10.50),
+        (4, 3, 9, 5, 50.99),
+        (5, 4, 9, 5, 50.99),
+        (6, 4, 6, 7, 2999.99),
+        (7, 6, 6, 5, 2999.99),
+        (8, 7, 3, 12, 15.99),
+        (9, 4, 5, 100, 4.50),
+        (10, 9, 5, 15, 4.50),
+        (11, 10, 1, 100, 2.99),
+        (12, 1, 2, 100, 10.50),
+        (13, 5, 6, 7, 2999.99),
+        (14, 8, 7, 20, 18.99);
